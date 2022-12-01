f816f71e957807413a30a9c809508d8df0f9fc10
benigne <benigne.demetz@etu.univ-amu.fr>
2022-12-01 15:22:30 +0100

    add unsigned print

---
+++ libft/ft_putunsigned_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdemetz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 18:44:21 by bdemetz           #+#    #+#             */
+/*   Updated: 2022/11/23 13:22:49 by bdemetz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	rcrsv_print(unsigned int n, int fd)
+{
+	int		i;
+	char	c;
+
+	i = n % 10;
+	c = i + 48;
+	if (n < 10)
+	{
+		write (fd, &c, 1);
+		return ;
+	}
+	rcrsv_print(n / 10, fd);
+	write (fd, &c, 1);
+}
+
+void	ft_putunsigned_fd(unsigned int n, int fd)
+{
+    rcrsv_print(n, fd);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -41,6 +41,7 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 void			ft_putstr_fd(char *s, int fd);
+void			ft_putunsigned_fd(unsigned int n, int fd);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 char			**ft_split(char const *s, char c);
